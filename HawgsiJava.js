0f35c5c3178db1e8dccd251230c3863f085289cb
Hawgsicle <73665211+Hawgsicle@users.noreply.github.com>
2024-02-23 13:19:32 -0600

    szregh

--- HawgsiJava.js
+++ HawgsiJava.js
@@ -4,7 +4,7 @@ function openCity(evt, cityName) {
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks";
+  tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace("active", "");
   }
@@ -12,3 +12,12 @@ function openCity(evt, cityName) {
   evt.currentTarget.className += "active";
 }
 document.getElementById("defaultOpen").click();
+
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
