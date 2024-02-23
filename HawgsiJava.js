0c3148022dbe0bcc0c90b16a0a29aaafacf5e957
Hawgsicle <73665211+Hawgsicle@users.noreply.github.com>
2024-02-23 13:02:45 -0600

    Update HawgsiJava.js

--- HawgsiJava.js
+++ HawgsiJava.js
@@ -1,10 +1,10 @@
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("hometabcontent","twitchtabcontent","patreontabcontent","hotlinetabcontent");
+  tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("hometablinks","patreontablinks","twitchtablinks","hotlinetablinks");
+  tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace("active", "");
   }
