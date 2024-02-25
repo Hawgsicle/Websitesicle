6b4c77a5719866e4eb64f1b1db744e67d131c542
Hawgsicle <73665211+Hawgsicle@users.noreply.github.com>
2024-02-24 18:48:43 -0600

    Update HawgsiJava.js

--- HawgsiJava.js
+++ HawgsiJava.js
@@ -1,23 +1,14 @@
 function openCity(evt, cityName) {
-  var i, tabcontent, homebutton;
+  var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("homebutton");
-  for (i = 0; i < homebutton.length; i++) {
-    homebutton[i].className = homebutton[i].className.replace("active", "");
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += "active";
 }
 document.getElementById("defaultOpen").click();
-
-function myFunction() {
-  var x = document.getElementById("myNavbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
