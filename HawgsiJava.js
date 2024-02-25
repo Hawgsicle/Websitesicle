2e174ed7fcec245a1ca75b1a6e18dfe02152418c
Hawgsicle <73665211+Hawgsicle@users.noreply.github.com>
2024-02-24 18:45:59 -0600

    Update HawgsiJava.js

--- HawgsiJava.js
+++ HawgsiJava.js
@@ -1,12 +1,12 @@
 function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
+  var i, tabcontent, homebutton;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace("active", "");
+  tablinks = document.getElementsByClassName("homebutton");
+  for (i = 0; i < homebutton.length; i++) {
+    homebutton[i].className = homebutton[i].className.replace("active", "");
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += "active";
